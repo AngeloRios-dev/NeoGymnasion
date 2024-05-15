491446c9f909becdba8d44162e072001bea8bf47
Angelo Rios <angelorios@live.com>
2024-05-15 21:22:45 +0200

    add: sections services and pricings

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
     <!-- CSS styles using bootstrap -->
     <link rel="stylesheet" href="resources/css/bootstrap.css">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="resources/css/style.css">
 
 </head>
 <body class="bg-dark">
     <header> <!-- seccion header -->
         <!-- barra de navegacion -->
-        <nav class="navbar navbar-dark navbar-expand-lg fixed-top">
+        <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark bg-gradient">
             <div class="container">
                 <a class="navbar-brand text-light fw-bold fs-1 text-uppercase" href="index.html"><span class="text-primary">Neo</span>Gymnasion</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -109,11 +109,11 @@
 </div> <!-- END carousel section -->
 
     <!-- Choose Your Destiny Section Begin -->
-    <section class="py-5">
+    <!-- <section class="py-5">
         <div class="container my-5">
             <div class="row">
                 <div class="col-lg-12 mb-5">
-                    <div class="section-title text-white text-uppercase text-center">
+                    <div class="text-white text-uppercase text-center">
                         <span class="text-primary">Elige tu destino</span>
                         <h2>Supera tus l&iacute;mites</h2>
                     </div>
@@ -139,77 +139,185 @@
                 </div>
             </div>
         </div>
-    </section> 
+    </section>  -->
     <!-- Choose Your Destiny Section End -->
 
     <!-- Classes section begin -->
-    <section class="py-5">
+    <section class="bg-dark">
         <div class="container my-5">
             <div class="row">
                 <div class="col-lg-12 my-5">
-                    <div class="section-title text-center text-uppercase text-white">
-                        <span class="text-primary">Clases</span>
-                        <h2>Lo que podemos ofrecerte</h2>
+                    <div class="text-center text-uppercase text-white">
+                        <span class="text-primary fw-bold">Clases</span>
+                        <h2>Lo que te ofrecemos</h2>
                     </div>
                 </div>
             </div>
 
-            <div class="row d-flex justify-content-center">
-                <div class="card col-lg-4 col-md-6 bg-secondary">
-                    <img src="resources/img/classes/class-1.jpg" class="car-img-top" alt="mujer tomando medidas">
-                    <div class="card-body">
-                        <h5 class="card-title">Entrenamiento Personalizado</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis!</p>
-                        <a href="#" class="btn btn-primary">Ver mas</a>
+            <div class="row gy-4">
+
+                <div class="col-12 col-lg-3">
+                    <div class="card h-100 shadow-sm border-0">
+                        <img src="resources/img/classes/class-1.jpg" class="card-img-top img-fluid" alt="imagen de franela destacada">
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Plan de Nutrici&oacute;n personalizado</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maxime provident atque dolor ducimus voluptate!
+                            </p>
+                            <a href="#" class="btn btn-dark mt-auto">Ver Mas</a>
+                        </div>
                     </div>
                 </div>
 
-                <div class="card col-lg-4 col-md-6 bg-secondary">
-                    <img src="resources/img/classes/class-2.jpg" alt="Mujer haciendo estiramientos" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Mejora tu flexibilidad</h5>
-                        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, optio?</p>
-                        <a href="#" class="btn btn-primary">Ver mas</a>
+                <div class="col-12 col-lg-3">
+                    <div class="card h-100 shasow-sm border-0">
+                        <img src="resources/img/classes/class-2.jpg" alt="Mujer haciendo ejercicios de flexibilidad" class="card-img-top img-fluid">
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Entrenador Personal</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maxime provident atque dolor ducimus voluptate!
+                            </p>
+                            <a href="#" class="btn btn-dark mt-auto">Ver Mas</a>
+                        </div>
                     </div>
                 </div>
 
-                <div class="card col-lg-4 col-md-6 bg-secondary">
-                    <img src="resources/img/classes/class-3.jpg" alt="Clase grupal" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Clases Grupales</h5>
-                        <p class="card-tex">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis!</p>
-                        <a href="#" class="btn btn-primary">Ver mas</a>
+                <div class="col-12 col-lg-3">
+                    <div class="card h-100 shadow-sm border-0">
+                        <img src="resources/img//classes/class-3.jpg" alt="tres personas entrenando" class="card-img-top img-fluid">
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Clases Grupales</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maxime provident atque dolor ducimus voluptate!
+                            </p>
+                            <a href="#" class="btn btn-dark mt-auto">Ver Mas</a>
+                        </div>
                     </div>
                 </div>
 
-            </div>
-            <div class="container">
-
-                <div class="row d-flex justify-content-center">
-                <div class="card col-lg-6 col-md-6 bg-secondary">
-                    <img src="resources/img/classes/class-4.jpg" alt="Hombre levantando pesas" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="cart-title">Entrenamientos de fuerza</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis!</p>
-                        <a href="#" class="btn btn-primary">Ver mas</a>
+                <div class="col-12 col-lg-3">
+                    <div class="card h-100 shadow-sm border-0">
+                        <img src="resources/img//classes/class-6.jpg" alt="tres personas entrenando" class="card-img-top img-fluid">
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Yoga e Inteligencia Emocional</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maxime provident atque dolor ducimus voluptate!
+                            </p>
+                            <a href="#" class="btn btn-dark mt-auto">Ver Mas</a>
+                        </div>
                     </div>
                 </div>
 
-                <div class="card col-lg-6 col-md-6 bg-secondary">
-                    <img src="resources/img/classes/class-5.jpg" alt="Clase de yoga" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Yoga</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis!</p>
-                        <a href="#" class="btn btn-primary">Ver mas</a>
+                <div class="col-12 col-lg-6">
+                    <div class="card h-100 shadow-sm border-0">
+                        <img src="resources/img//classes/class-4.jpg" alt="Hombre levantando pesas" class="card-img-top img-fluid">
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Entrenamientos de fuerza</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maxime provident atque dolor ducimus voluptate!
+                            </p>
+                            <a href="#" class="btn btn-dark mt-auto">Ver Mas</a>
+                        </div>
                     </div>
                 </div>
-    
+
+                <div class="col-12 col-lg-6">
+                    <div class="card h-100 shadow-sm border-0">
+                        <img src="resources/img/classes/class-5.jpg" alt="dos boxeadores" class="card-img-top img-fluid">
+                        <div class="card-body d-flex flex-column">
+                            <h3 class="card-title">Clases de Combate</h3>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maxime provident atque dolor ducimus voluptate!
+                            </p>
+                            <a href="#" class="btn btn-dark mt-auto">Ver Mas</a>
+                        </div>
+                    </div>
                 </div>
+
             </div>
 
         </div>
     </section>
     <!-- Classes section end -->
+
+    <!-- Appointments Section Begin -->
+    <section class="mt-5 pt-5">
+        <div class="appointment-section set-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center text-white text-uppercase">
+                        <h2>Asesorate con nuestros expertos</h2>
+                        <a href="#" class="btn btn-outline-primary">Cita Previa</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Appointments Section End -->
+
+    <!-- Pricing Section Begin -->
+    <section class="bg-secondary py-5">
+        <div class="container">
+            <div class="row col-lg-12 mb-5">
+                <div class="text-center text-uppercase text-white">
+                    <span class="text-dark fw-bold">Nuestros Planes</span>
+                    <h2>Escoje el plan que mejor te convenga</h2>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-12 col-lg-4">
+                    <div class="card h-100 shadow-sm border-0">
+                        <div class="card-header fw-bold text-center">Plan B&aacute;sico - "Iniciado"</div>
+                        <div class="card-body d-flex flex-column">
+                            <h2 class="card-title fw-bold text-center">&euro;50</h2>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Clases grupales (Muay Thai, MMA, BJJ, Kyokushinkai Karate)</li>
+                                <li class="list-group-item">Acceso a entrenamientos de fuerza</li>
+                                <li class="list-group-item">Acceso a clases de yoga e inteligencia emocional</li>
+                                <li class="list-group-item">Sesiones semanales de meditación guiada</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary mt-auto">Enl&iacute;state Ahora</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-4">
+                    <div class="card h-100 shadow-sm border-0">
+                        <div class="card-header fw-bold text-center">Plan Intermedio - "Guerrero"</div>
+                        <div class="card-body d-flex flex-column">
+                            <h2 class="cart-title fw-bold text-center">&euro;80</h2>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Todo lo incluido en el Plan Básico</li>
+                                <li class="list-group-item">Plan de nutrición personalizado</li>
+                                <li class="list-group-item">Sesiones de entrenamiento personalizadas</li>
+                                <li class="list-group-item">Sesiones bi-semanales de meditación guiada</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary mt-auto">Enl&iacute;state Ahora</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-4">
+                    <div class="card h-100 shadow-sm border-0">
+                        <div class="card-header fw-bold text-center">Plan Avanzado - "Heroico"</div>
+                        <div class="card-body d-flex flex-column">
+                            <h2 class="cart-title fw-bold text-center">&euro;120</h2>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Todo lo incluido en los planes anteriores</li>
+                                <li class="list-group-item">Acceso ilimitado a entrenamientos en combate</li>
+                                <li class="list-group-item">Asesoramiento adicional de salud y bienestar</li>
+                                <li class="list-group-item">Sesiones ilimitadas de meditación guiada</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary mt-auto">Enl&iacute;state Ahora</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Pricing Section End -->
     
 
     <footer class="container">
