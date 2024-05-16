3fe74bb83f312d43ea331ba7e4e793cc8664b122
Angelo Rios <angelorios@live.com>
2024-05-16 15:36:43 +0200

    fix: move header and footer to includes files

---
+++ includes/visitante_footer.php
@@ -0,0 +1,26 @@
+    <!-- Footer section Begin -->
+    <footer class="container">
+        <div class="row my-5 pt-3 border-top border-secondary">
+            <div class="col-md-6 text-white">
+                <p class="text-center text-md-start">
+                    <span class="text-uppercase fw-bold"><span class="text-primary">Neo</span>Gymnasion</span>
+                    &copy;<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados.
+                </p>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center justify-content-md-end">
+                <a href="https://twitter.com/" target="_blank"><i class="bi bi-twitter mx-1"></i></a>
+
+                <a href="https://www.facebook.com/" target="_blank"><i class="bi bi-facebook mx-1"></i></a>
+
+                <a href="https://www.linkedin.com/" target="_blank"><i class="bi bi-linkedin mx-1"></i></a>
+
+                <a href="https://www.instagram.com/" target="_blank"><i class="bi bi-instagram mx-1"></i></a>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer section End -->
+
+    <!-- Loading Boostrap JS bundle -->
+    <script src="resources/js/bootstrap.bundle.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,69 +1,15 @@
-<?php $page_title = 'Bienvenidos';?>
+<?php 
+// Set page title
+$page_title = 'Bienvenidos';
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="En NeoGymnasion, nos inspiramos en la fortaleza y disciplina de los antiguos guerreros espartanos y luchadores griegos, así como en los principios del estoicismo, para crear un espacio único donde el cuerpo y la mente se fortalecen en armonía.">
-    <meta name="keywords" content="gimnacio, disciplina, guerrero, estoicismo, salud, armonia">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $page_title; ?> | NeoGymnasion</title>
-
-    <!-- CSS styles using bootstrap -->
-    <link rel="stylesheet" href="resources/css/bootstrap.css">
-    <!-- Iconos Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="resources/css/style.css">
-
-</head>
-<body class="bg-dark">
-    <header> <!-- seccion header -->
-        <!-- barra de navegacion -->
-        <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark bg-gradient">
-            <div class="container">
-                <a class="navbar-brand text-light fw-bold fs-1 text-uppercase" href="index.html"><span class="text-primary">Neo</span>Gymnasion</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse bg-clr-500" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item px-2">
-                            <a class="nav-link" aria-current="page" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item px-2">
-                            <a class="nav-link" href="./views/clases.php">Clases</a>
-                        </li>
-                        <li class="nav-item px-2">
-                            <a class="nav-link" href="./views/noticias.php">Noticias</a>
-                        </li>
-                        <li class="nav-item px-2">
-                            <a class="nav-link" href="./views/citas.php">Citas</a>
-                        </li>
-                        <li class="nav-item px-2">
-                            <a class="nav-link" href="./views/perfil.php">Perfil</a>
-                        </li>
-                        <li class="nav-item px-2">
-                            <a class="nav-link" href="./views/logout.php">Cerrar Sesi&oacute;n</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav> <!-- FIN barra de navegacion -->
+// Include header for unauthenticated users
+require('includes/visitante_header.php');
+?>
 
 
-    </header> <!-- FIN seccion header -->
 
 <!-- carousel section -->
 <div id="carouselE" class="carousel slide" data-bs-ride="carousel">
-
-    <!-- carousel indicator buttons -->
-    <!-- <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselE" data-bs-slide-to="0" class="active" aria-current="true" aria-label="1"></button>
-        <button type="button" data-bs-target="#carouselE" data-bs-slide-to="1" aria-current="true" aria-label="2"></button>
-
-    </div>  -->
-    <!-- END carousel indicator buttons -->
 
     <!-- Carousel images -->
     <div class="carousel-inner">
@@ -266,7 +212,7 @@
                     <h2>Escoje el plan que mejor te convenga</h2>
                 </div>
             </div>
-            <div class="row">
+            <div class="row d-flex gap-4 gap-lg-0">
 
                 <div class="col-12 col-lg-4">
                     <div class="card h-100 shadow-sm border-0">
@@ -328,24 +274,8 @@
     <!-- Pricing Section End -->
     
 
-    <footer class="container">
-        <div class="row">
-            <div class="col-md-6 text-white">
-                <p class="text-center text-md-start">
-                    &copy;<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados.
-                </p>
-            </div>
-            <div class="col-md-6">
-                <a href="https://twitter.com/" target="_blank"><i class="bi bi-twitter mx-1"></i></a>
 
-                <a href="https://www.facebook.com/" target="_blank"><i class="bi bi-facebook mx-1"></i></a>
-
-                <a href="https://www.linkedin.com/" target="_blank"><i class="bi bi-linkedin mx-1"></i></a>
-
-                <a href="https://www.instagram.com/" target="_blank"><i class="bi bi-instagram mx-1"></i></a>
-            </div>
-        </div>
-    </footer>
-    <script src="resources/js/bootstrap.bundle.js"></script>
-</body>
-</html>
+<?php 
+// Include Footer for unauthenticated users
+require('includes/visitante_footer.php');
+?>
